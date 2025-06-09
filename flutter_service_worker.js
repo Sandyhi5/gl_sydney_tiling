16fa565b95aba04b4fd03e80f30e9b6d507cbc1a
SandyHi5 <sandeep.chandra@ymail.coom>
2025-06-09 19:15:08 +1000

    Intro of MobileView

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,14 +51,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "214feae6f500f5052c51410e2acb1bf5
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "9a07cbb19a0ad159b3dee25e07b15713",
+"flutter_bootstrap.js": "30984822b820945d113b91a65a4c3b43",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "76aff4a8900f1d7ce39dd210135799ef",
 "/": "76aff4a8900f1d7ce39dd210135799ef",
-"main.dart.js": "6d48c0a8d0142d85da6eeec303ce8108",
+"main.dart.js": "5eca46be4796299b79964344b5d72768",
 "manifest.json": "2897c9afbe459ba6d232ef02482a3431",
 "version.json": "d1b3423f8692d6f246b682c54bf6b951"};
 // The application shell files that are downloaded before a service worker can
