bbe42ee2d80700b28b33855279a9dd1a6c2c37c4
SandyHi5 <sandeep.chandra@ymail.coom>
2025-06-19 19:17:20 +1000

    Minor changes after clone

--- lib/EnquirySection.dart
+++ lib/EnquirySection.dart
@@ -6,7 +6,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:responsive_builder/responsive_builder.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:emailjs/emailjs.dart' as emailjs;
 
